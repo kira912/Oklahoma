85f41d628af58ec71d4db7e78f82df3eca651e3b
Valentin Perot <perot.valentin@gmail.com>
2017-09-16 01:05:30 +0200

    mouvement joueur

--- Oklahoma.js
+++ Oklahoma.js
@@ -11,53 +11,111 @@ function Oklahoma() {
       [null, null,null,null], // 8
       [null, 'A',null,null]  // 9
     ];
+    // Joueur defenseur
     this.defense = {
       name: 'D',
       row: 6,
       col: 1
     };
+    // Joueur attaquant
     this.attack = {
       name: 'A',
       row: 9,
       col: 1
     };
 }
+
+// Affichage du terrain
 Oklahoma.prototype.display = function () {
   this.field.forEach(function(row) {
     console.log(row);
   });
 };
 
-Oklahoma.prototype.moveForwardAttack = function () {
-  if (this.field[this.attack.row - 1][this.attack.col] === null) {
-    this.field[this.attack.row - 1][this.attack.col] = this.attack.name;
-    this.field[this.attack.row][this.attack.col] = null;
-    this.attack.row--;
-    this.display();
+// Fonction faire avancer un joueur
+Oklahoma.prototype.moveForward = function (team, increment) {
+  if (this.field[this[team].row + increment][this[team].col] === null) {
+    this.field[this[team].row + increment][this[team].col] = this[team].name;
+    this.field[this[team].row][this[team].col] = null;
   }
-  else if (this.field[this.attack.row-1][this.attack.col] === 'TD') {
+  this.display();
+
+   if (this.field[this.attack.row-1][this.attack.col] === 'TD') {
     console.log("TOUCHDOOOOOOOOOOOOWN");
   }
 };
 
+// Fontion faire avancer joueur attaquant
+Oklahoma.prototype.moveForwardAttack = function() {
+  this.moveForward("attack",-1);
+  this.attack.row--;
+};
+
+// Fonction faire avancer joueur defenseur
+Oklahoma.prototype.moveForwardDefense = function () {
+  this.moveForward("defense",1);
+  this.defense.row++;
+};
+
+// Fonction deplacer joueur vers la gauche
+Oklahoma.prototype.moveLeft = function (team, increment) {
+  if (this.field[this[team].row][this[team].col + increment] === null) {
+    this.field[this[team].row][this[team].col + increment] = this[team].name;
+    this.field[this[team].row][this[team].col] = null;
+  }
+
+  this.display();
+};
+
+// Fonction faire deplacer le joueur attaquant
 Oklahoma.prototype.moveLeftAttack = function () {
-  if (this.field[this.attack.row][this.attack.col - 1] === null) {
-    this.field[this.attack.row][this.attack.col - 1] = this.attack.name;
-    this.field[this.attack.row][this.attack.col] = null;
-    this.attack.col--;
+  this.moveLeft("attack", -1);
+  this.attack.col--;
+};
+
+// Fonction faire deplacer le joueur defenseur sur la gauche
+Oklahoma.prototype.moveLeftDefense = function() {
+  this.moveLeft("defense", -1);
+  this.defense.col--;
+};
+
+// Fonction faire deplacer un joueur vers la droite
+Oklahoma.prototype.moveRight = function (team, increment) {
+  if (this.field[this[team].row][this[team].col + increment] === null ) {
+    this.field[this[team].row][this[team].col + increment] = this[team].name;
+    this.field[this[team].row][this[team].col] = null;
   }
   this.display();
 };
 
+// Fonction faire deplacer le joueur attauqant vers la droite
 Oklahoma.prototype.moveRightAttack = function () {
-  if (this.field[this.attack.row][this.attack.col + 1] === null ) {
-    this.field[this.attack.row][this.attack.col + 1] = this.attack.name;
-    this.field[this.attack.row][this.attack.col] = null;
-    this.attack.col ++;
+  this.moveRight("attack", 1);
+  this.attack.col++;
+};
+
+// Fonction faire depalcer le joueur defenseur vers la droite
+Oklahoma.prototype.moveRightDefense = function () {
+  this.moveRight("defense", 1);
+};
+
+// Fonction faire reculer un joueur
+Oklahoma.prototype.moveBackward = function (team, increment) {
+  if (this.field[this[team].row + increment][this[team].col] === null){
+      this.field[this[team].row + increment][this[team].col] = this[team].name;
+      this.field[this[team].row][this[team].col] = null;
   }
   this.display();
 };
 
+// Fonction faire reculer le joueur attaquant
 Oklahoma.prototype.moveBackwardAttack = function () {
+  this.moveBackward("attack", 1);
+  this.attack.row++;
+};
 
+// Fonction faire reculer le joueur defenseur
+Oklahoma.prototype.moveBackwardDefense = function () {
+  this.moveBackward("defense", -1);
+  this.defense.col--;
 };
