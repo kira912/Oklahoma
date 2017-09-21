ebf91365ea1323384839b2fdbd77c7a891d6ca28
Valentin Perot <perot.valentin@gmail.com>
2017-09-21 15:00:33 +0200

    mouvement simultané, finish, gif

--- js/Oklahoma.js
+++ js/Oklahoma.js
@@ -1,15 +1,15 @@
 function Oklahoma() {
 
     // x est compris entre 0 et 48.8
-    var maxX = 48.8;
-    var maxY = 110.0;
+    var maxX = 170;
+    var maxY = -270;
     // y est compris entre 0 et 110
 
     // Joueur defenseur
     this.defense = {
       name: 'D',
       x: maxX / 2,
-      y: maxY / 5,
+      y: -100,
       radius: 10,
       move: null
     };
@@ -28,21 +28,19 @@ Oklahoma.prototype.move = function () {
 
 // Mouvement attaquant
   switch (this.attack.move) {
-    case "up": this.attack.y -= 2.5; break;
-    case "down": this.attack.y += 2.5; break;
-    case "left": this.attack.x -= 2.5; break;
-    case "right": this.attack.x += 2.5; break;
+    case "up": this.attack.y -= 1.5; break;
+    case "down": this.attack.y += 1.5; break;
+    case "left": this.attack.x -= 1.5; break;
+    case "right": this.attack.x += 1.5; break;
   }
-  this.attack.move = null;
 
 // Mouvement defense
   switch (this.defense.move) {
-    case "up": this.defense.y -= 2.5; break;
-    case "down": this.defense.y += 2.5; break;
-    case "left": this.defense.x -= 2.5; break;
-    case "right": this.defense.x += 2.5; break;
+    case "up": this.defense.y -= 1.8; break;
+    case "down": this.defense.y += 1.8; break;
+    case "left": this.defense.x -= 1.8; break;
+    case "right": this.defense.x += 1.8; break;
   }
-  this.defense.move = null;
 };
 
 // Enregistre mouvement attaquant
@@ -55,24 +53,46 @@ Oklahoma.prototype.registerMoveDefense = function (move) {
   this.defense.move = move;
 };
 
+Oklahoma.prototype.unregisterMoveAttack = function (move) {
+  if (this.attack.move === move) {
+    this.attack.move = null;
+  }
+};
+
+Oklahoma.prototype.unregisterMoveDefense = function (move) {
+  if (this.defense.move === move) {
+    this.defense.move = null;
+  }
+};
+
 Oklahoma.prototype.testCollision = function () {
     var detectionX = (this.attack.x + this.attack.radius) - (this.defense.x + this.defense.radius);
     var detectionY = (this.attack.y + this.attack.radius) - (this.defense.y + this.defense.radius);
     var distance = Math.sqrt(detectionX * detectionX + detectionY * detectionY);
 
     if (distance < this.attack.radius + this.defense.radius) {
-      console.log("collision");
+      return true;
     }
+    return false;
 };
 
 Oklahoma.prototype.testSortie = function () {
-  if (this.attack.x > 118 || this.attack.x < 2) {
-    console.log("OUT");
+  if (this.attack.x > 160 || this.attack.x < 15) {
+    return true;
   }
+  return false;
+};
+
+Oklahoma.prototype.testTouchdown = function () {
+  if (this.attack.y < - (270)) {
+    return true;
+  }
+  return false;
 };
 
 Oklahoma.prototype.tick = function (timeLapsted) {
   this.move();
   this.testCollision();
   this.testSortie();
+  this.testTouchdown();
 };
--- js/main.js
+++ js/main.js
@@ -2,22 +2,58 @@ var game;
 $(document).ready(function() {
   var divP1 = $("#player-one");
   var divP2 = $("#player-two");
+    var finish = $("#win");
 
   function display () {
     var attackPx = playerPx(game.attack);
     divP1.css("transform", "translate(" + attackPx.x + "," + attackPx.y + ")");
     var defensePx = playerPx(game.defense);
     divP2.css("transform", "translate(" + defensePx.x + "," + defensePx.y + ")");
+
+    if (game.testCollision()) {
+      finish.show();
+      $("#message").text("Big hit defense !");
+      $("#team-win").text("Defense");
+      $("#gif").attr("src","css/gif/gifHit.gif");
+      ongoing = false;
+    }
+
+    if (game.testSortie()) {
+      finish.show();
+      $("#message").text("Out of the field");
+      $("#team-win").text("Defense");
+      $("#gif").attr("src","css/gif/gifOut.gif");
+      ongoing = false;
+    }
+
+    else if (game.testTouchdown()) {
+      finish.show();
+      $("#message").text("touchdown");
+      $("#team-win").text("Attack");
+      $("#gif").attr("src","css/gif/gifTD.gif");
+      $("#sound-match").stop();
+      ongoing = false;
+    }
   }
 
+  $("#restart").click(function() {
+    game = new Oklahoma();
+    finish.hide();
+    ongoing = true;
+    gameLoop();
+  });
+
   $(document).keydown(moveListeners);
+  $(document).keyup(unmoveListeners);
   game = new Oklahoma();
   display();
   var previousTime;
   var currentTime = Date.now();
   var ongoing = true;
+
   function gameLoop() {
-    if (!ongoing) return;
+    if (!ongoing)
+    return;
     previousTime = currentTime;
     currentTime = Date.now();
     game.tick(currentTime - previousTime);
@@ -26,11 +62,7 @@ $(document).ready(function() {
   }
   requestAnimationFrame(gameLoop);
 
-  var finishHit = $("#defense-win");
-  finishHit.hide();
-  if (game.testCollision()) {
-    finishHit.toggle();
-  }
+  finish.hide();
 });
 
 
@@ -39,6 +71,10 @@ function playerPx(player) {
     x: String(player.x * 2) + 'px',
     y: String(player.y * 2) + 'px'
   };
+}
+
+function logkey(event){
+  console.log(event.type + ' ' + event.keyCode);
 }
 
 
@@ -66,6 +102,33 @@ function moveListeners (event) {
      case 83: game.registerMoveDefense("down");
      break;
      case 90: game.registerMoveDefense("up");
+     break;
+  }
+}
+function unmoveListeners (event) {
+  var attackKeys = [37, 38, 39, 40];
+  var defenseKeys = [68, 81, 83, 90];
+  var keys = attackKeys.concat(defenseKeys);
+  if (keys.indexOf(event.keyCode) < 0) {
+    return;
+  }
+
+  switch (event.keyCode) {
+    case 37: game.unregisterMoveAttack("left");
+    break;
+    case 38: game.unregisterMoveAttack("up");
+    break;
+    case 39: game.unregisterMoveAttack("right");
+    break;
+    case 40: game.unregisterMoveAttack("down");
+     break;
+     case 68: game.unregisterMoveDefense("right");
+     break;
+     case 81: game.unregisterMoveDefense("left");
+     break;
+     case 83: game.unregisterMoveDefense("down");
+     break;
+     case 90: game.unregisterMoveDefense("up");
      break;
   }
 }
